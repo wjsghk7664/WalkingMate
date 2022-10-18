17f3e2164d92c39f75aebaa2bc32a763210c4c11
AndroidUser <wjsghk7664@naver.com>
2022-10-18 23:11:41 +0900

    set chatting

---
+++ app/src/main/java/com/example/walkingmate/ChattingActivity.java
@@ -0,0 +1,14 @@
+package com.example.walkingmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChattingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting);
+    }
+}
--- app/src/main/java/com/example/walkingmate/MainActivity.java
+++ app/src/main/java/com/example/walkingmate/MainActivity.java
@@ -57,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.chatbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ChattingActivity.class));
+            }
+        });
+
 
 
 
