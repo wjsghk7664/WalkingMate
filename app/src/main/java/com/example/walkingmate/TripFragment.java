31b640a6d09bf6cbb99884dfb18bdb01fd01defb
AndroidUser <wjsghk7664@naver.com>
2022-11-08 18:51:26 +0900

    .

--- app/src/main/java/com/example/walkingmate/TripFragment.java
+++ app/src/main/java/com/example/walkingmate/TripFragment.java
@@ -1,82 +1,147 @@
 package com.example.walkingmate;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
+
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TripFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TripFragment extends Fragment {
-    private RecyclerView mRecyclerView;
-    private RecyclerViewAdapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-    private ArrayList<RecyclerViewItem> mList;
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference databaseReference;
+    FirebaseFirestore fb=FirebaseFirestore.getInstance();
+    CollectionReference tripdata=fb.collection("tripdatalist");
+
+    ListView triplist;
+    TripAdapter tripAdapter;
+
+    ImageButton addtrip;
+    String curitem="0";
+
+    ArrayList<String> tripdocuids=new ArrayList<>();
+    HashMap<String,String> titles=new HashMap<>();
+    HashMap<String, String> dates=new HashMap<>();
+    HashMap<String, ArrayList<String>> locations=new HashMap<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_trip, container, false);
 
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(layoutManager);
-        mList = new ArrayList<>();
-
-        firebaseDatabase = FirebaseDatabase.getInstance();
-
-        databaseReference = firebaseDatabase.getReference("Listdata");
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                mList.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    RecyclerViewItem item = snapshot.getValue(RecyclerViewItem.class);
-                    mList.add(item);
-                }
-                mAdapter.notifyDataSetChanged();
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("BoardListActivity", String.valueOf(error.toException()));
-
-            }
-        });
-
-        mAdapter = new RecyclerViewAdapter(mList,getActivity());
-        mRecyclerView.setAdapter(mAdapter);
-
-       view.findViewById(R.id.gomain_trip).setOnClickListener(new View.OnClickListener() {
+        triplist=view.findViewById(R.id.triplist);
+        addtrip=view.findViewById(R.id.add_triplist);
+        addtrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(),MainActivity.class));
+                startActivity(new Intent(getActivity(),TripwriteActivity.class));
             }
         });
 
+
+
         return view;
+    }
+
+    public void getlist(){
+        tripdata.whereGreaterThan("writetime",curitem).orderBy("writetime").limit(25).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(!task.getResult().isEmpty()){
+                    QuerySnapshot documents=task.getResult();
+                    for(DocumentSnapshot document: documents){
+                        tripdocuids.add(document.getId());
+                        titles.put(document.getId(), (String) document.get("title"));
+                        try {
+                            dates.put(document.getId(),getdatestr(document.get("year"),document.get("month"),document.get("day"),document.get("hour"),document.get("minute"),document.get("takentime")));
+                        } catch (ParseException e) {
+                            dates.put(document.getId()," ");
+                        }
+                        locations.put(document.getId(), (ArrayList<String>) document.get("locations_name"));
+                    }
+                }
+            }
+        });
+    }
+
+    public String getdatestr(Object y, Object m, Object d, Object h, Object min, Object taken) throws ParseException {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy/MM/dd hh:mm");
+        String start=String.format("%04d/%02d/%02d %02d:%02d",y,m,d,h,min);
+        Date date=sdf.parse(start);
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MINUTE,(int)taken);
+        return sdf.format(calendar.getTime());
+    }
+
+
+    public class TripAdapter extends BaseAdapter {
+
+        LayoutInflater layoutInflater;
+
+
+        public TripAdapter(Context context){
+            this.layoutInflater = LayoutInflater.from(context);
+
+        }
+        @Override
+        public int getCount() {
+            return tripdocuids.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = layoutInflater.inflate(R.layout.list_layout, null);
+            View emptyview=layoutInflater.inflate(R.layout.list_layout_empty,null);
+            if(tripdocuids.size()==0){
+                return emptyview;
+            }
+
+            View bodyView = view.findViewById(R.id.trip_body);
+            bodyView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent=new Intent(getActivity(),TripViewActivity.class);
+                    intent.putExtra("docuid",tripdocuids.get(position));
+                    startActivity(intent);
+                }
+            });
+
+
+            return view;
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/walkingmate/TripViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.walkingmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TripViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_view);
+    }
+}
